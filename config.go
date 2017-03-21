642e497229872ad8ed0cfbbe9e6230c7c32ce760
Gordon Mackenzie-Leigh <gordon@stugo.co.uk>
2017-03-21 15:55:54 +0000

    remove unnecessary viper compat code (use viper.GetViper() instead)

--- config.go
+++ config.go
@@ -2,8 +2,6 @@ package goconfig
 
 import (
 	"time"
-
-	"github.com/spf13/viper"
 )
 
 // Config is the interface used by subpackages to retrieve config values.
@@ -17,36 +15,4 @@ type Config interface {
 	GetTime(key string) time.Time
 	GetDuration(key string) time.Duration
 	IsSet(key string) bool
-}
-
-type vpr struct{}
-
-// Viper creates a config provider from github.com/spf13/viper
-func Viper() Config {
-	return &vpr{}
-}
-
-func (vpr) GetBool(key string) bool {
-	return viper.GetBool(key)
-}
-func (vpr) GetInt(key string) int {
-	return viper.GetInt(key)
-}
-func (vpr) GetFloat64(key string) float64 {
-	return viper.GetFloat64(key)
-}
-func (vpr) GetString(key string) string {
-	return viper.GetString(key)
-}
-func (vpr) GetStringSlice(key string) []string {
-	return viper.GetStringSlice(key)
-}
-func (vpr) GetTime(key string) time.Time {
-	return viper.GetTime(key)
-}
-func (vpr) GetDuration(key string) time.Duration {
-	return viper.GetDuration(key)
-}
-func (vpr) IsSet(key string) bool {
-	return viper.IsSet(key)
 }
